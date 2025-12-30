c872a30119dc861fa289b9052cfeaefcb8ed3241
Sai Gautam Neela <saigautam.neela@gmail.com>
2025-12-30 23:30:42 +0530

    keys removed from version control

--- final_project_modified/tailwindcss4/src/pinataConfig.js
+++ final_project_modified/tailwindcss4/src/pinataConfig.js
@@ -7,5 +7,5 @@
 // export const PINATA_SECRET_API_KEY = "your_actual_pinata_secret_api_key";
 
 
-export const PINATA_API_KEY="c32c8709790bda0d2375";
-export const PINATA_SECRET_API_KEY="73da911f0364a28409871f5d9900c85b86b6acd088e41c53408632473462cd26";
+export const PINATA_API_KEY="add the key here";
+export const PINATA_SECRET_API_KEY="add the secret key here";
